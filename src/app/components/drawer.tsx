0e3ea034cdc8f61ea4f42f4a70a3ca461ff781e5
Mateo Cetti <mateocetti2000@gmail.com>
2024-07-19 17:22:22 -0300

    Finish drawer component

--- src/app/components/drawer.tsx
+++ src/app/components/drawer.tsx
@@ -2,19 +2,39 @@
 import { useState } from "react";
 
 import MenuIcon from "../icons/menu";
+import { menuItems } from "./navbar";
+import Link from "next/link";
 
-export default function Drawer() {
+type sape = typeof menuItems;
+
+export default function Drawer({ items }: { items: sape }) {
     const [visible, setVisible] = useState<boolean>(false);
 
-    function changeVisibleState() {
-        const newVisible = !visible;
-        setVisible(newVisible)
+    function changeVisibleState(state: boolean) {
+        console.log(state)
+        const newVisible: boolean = state;
+        setVisible(newVisible);
     }
     return (
         <>
-            <MenuIcon className="col-span-1 lg:hidden self-center" width={25} height={25} onClick={() => { changeVisibleState() }} />
-            <div className={`transition ${visible ? "translate-x-0" : "-translate-x-80"} duration-1000 bg-white fixed w-7/12 h-screen z-10`} ></div>
-            <div className={`transition ${visible ? "opacity-80" : "opacity-0"} duration-1000 bg-black fixed w-screen h-screen end-0 z-0`} onClick={() => { changeVisibleState() }}></div>
+            <MenuIcon className="col-span-1 lg:hidden self-center" width={25} height={25} onClick={() => { changeVisibleState(true) }} />
+            <div className={`transition ${visible ? "translate-x-0" : "-translate-x-full"} duration-1000 bg-white fixed w-7/12 h-screen z-10`} >
+                <div className="flex flex-col gap-10 text-center">
+                    <p className="text-xl mt-5 border-b-4">Menu</p>
+                    {
+                        items.map((element, index) =>
+                            <Link
+                                key={index}
+                                onClick={() => { changeVisibleState(false) }}
+                                href={element.href}>{element.name}
+                            </Link>)
+                    }
+                </div>
+            </div>
+            <div
+                className={`transition ${visible ? "opacity-60" : "hidden"} duration-1000 bg-black fixed w-screen h-screen end-0`}
+                onClick={() => { changeVisibleState(false) }}>
+            </div>
         </>
     );
 }
--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -2,22 +2,34 @@ import Image from "next/image";
 import Link from "next/link";
 import Drawer from "./drawer";
 
+export const menuItems: Array<{ name: string, href: string }> = [
+    { name: "Inicio", href: "/" },
+    { name: "Productos", href: "/products" },
+    { name: "Sobre nosotros", href: "/about_us" },
+    { name: "Donde ubicarnos", href: "/find_us" }
+]
+
 export default function Navbar() {
-    
+
     return (
         <nav className="grid grid-cols-3 p-2 bg-white shadow-md fixed top-0 w-screen">
-                <Drawer />
-                <Image className="col-span-2 lg:col-span-1"
-                    src="/logo_1.png"
-                    width={94}
-                    height={43}
-                    alt="Picture of the author" />
-                <ul className="hidden lg:flex flex-row justify-around mt-2.5">
-                    <li><Link href={"/"}>Inicio</Link></li>
-                    <li><Link href={"/products"}>Productos</Link></li>
-                    <li><Link href={"/about_us"}>Sobre nosotros</Link></li>
-                    <li><Link href={"/find_us"}>Donde ubicarnos</Link></li>
-                </ul>
+            <Drawer items={menuItems} />
+            <Image className="col-span-2 lg:col-span-1"
+                src="/logo_1.png"
+                width={94}
+                height={43}
+                alt="Picture of the author" />
+            <ul className="hidden lg:flex flex-row justify-around mt-2.5">
+                {
+                    menuItems.map((element, index) =>
+                        <li>
+                            <Link
+                                key={index}
+                                href={element.href}>{element.name}
+                            </Link>
+                        </li>)
+                }
+            </ul>
         </nav>
     );
 }
