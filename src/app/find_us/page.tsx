457488a622de69108df8e74405da5c8ceaec289e
Mateo Cetti <mateocetti2000@gmail.com>
2024-07-20 14:41:30 -0300

    Fix build error

--- src/app/find_us/page.tsx
+++ src/app/find_us/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+
 export default function FindUsPage() {
     return (
         <section className="bg-white mt-20 mb-5">
@@ -13,7 +14,7 @@ export default function FindUsPage() {
                 <div className=" p-10 flex justify-center">
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27246.59763267547!2d-64.13171686523438!3d-31.39139669999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x943297d6ff1e437d%3A0xc93a4dc6b8bb8848!2sMercado%20de%20Abasto!5e0!3m2!1ses-419!2sar!4v1721495073045!5m2!1ses-419!2sar"
-                        width={visualViewport?.width}
+                        width={"100%"}
                         height="450"
                         allowFullScreen
                         loading="lazy"
