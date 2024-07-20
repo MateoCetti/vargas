eebcd34fad80cbf1a4578ae7a5c131ee394fac23
Mateo Cetti <mateocetti2000@gmail.com>
2024-07-20 14:32:58 -0300

    Add about us page.

--- src/app/find_us/page.tsx
+++ src/app/find_us/page.tsx
@@ -1,6 +1,25 @@
-
-export default function FindUsPage(){
+"use client"
+export default function FindUsPage() {
     return (
-        <>hola</>
+        <section className="bg-white mt-20 mb-5">
+            <div className="bg-green-900 text-center py-4 text-white text-xl w-full">¿Donde encontrarnos?</div>
+            <div className="flex grid grid-cols-1 lg:grid-cols-2">
+                <div className="text-justify lg:text-center p-5 self-center">
+                    <p>Estamos ubicados en el mercado de abasto de la provincia de Córdoba
+                        (Ruta 19, Km 7 1/2, Córdoba Argentina), mas esepcificamente hablando
+                        en la nave 3, puesto 322.
+                    </p>
+                </div>
+                <div className=" p-10 flex justify-center">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27246.59763267547!2d-64.13171686523438!3d-31.39139669999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x943297d6ff1e437d%3A0xc93a4dc6b8bb8848!2sMercado%20de%20Abasto!5e0!3m2!1ses-419!2sar!4v1721495073045!5m2!1ses-419!2sar"
+                        width={visualViewport?.width}
+                        height="450"
+                        allowFullScreen
+                        loading="lazy"
+                        referrerPolicy="no-referrer-when-downgrade" />
+                </div>
+            </div>
+        </section>
     );
 }
