1e2e9cfc5c6145aac575f2cedaa96b27edd7f975
Mateo Cetti <mateocetti2000@gmail.com>
2024-07-24 15:03:08 -0300

    Fix navbar behind product image bug.

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -14,7 +14,7 @@ export type itemsType = typeof menuItems;
 export default function Navbar() {
 
     return (
-        <nav className="grid grid-cols-3 p-2 bg-white shadow-md fixed top-0 w-screen">
+        <nav className="grid grid-cols-3 p-2 bg-white shadow-md fixed top-0 w-screen z-50">
             <Drawer items={menuItems} />
             <Image className="col-span-2 lg:col-span-1"
                 src="/logo_1.png"
