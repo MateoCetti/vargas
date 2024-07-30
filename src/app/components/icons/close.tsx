ba64b10189283e7b98bcd2d38ea8a586a65450f0
Mateo Cetti <mateocetti2000@gmail.com>
2024-07-30 07:43:09 -0300

    Add close icon.

---
+++ src/app/components/icons/close.tsx
@@ -0,0 +1,16 @@
+import * as React from "react"
+import { SVGProps } from "react"
+export default function CloseIcon(props: SVGProps<SVGSVGElement>) {
+    return (
+        <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={24}
+        height={24}
+        viewBox="0 -960 960 960"
+        {...props}
+      >
+        <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+      </svg>
+    );
+}
+
