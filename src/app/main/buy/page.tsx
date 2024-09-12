3211bb8caa07e4e5fac71916039b7450f8d114c3
Mateo Cetti <mateocetti2000@gmail.com>
2024-09-12 19:25:25 -0300

    Add quantity changer to cart page

--- src/app/main/buy/page.tsx
+++ src/app/main/buy/page.tsx
@@ -3,17 +3,37 @@
 import Image from "next/image";
 import Link from "next/link";
 
-import { useAppSelector } from "@/lib/hooks";
+import { useAppDispatch, useAppSelector } from "@/lib/hooks";
 import { getImageId } from "@/utils";
 import BackIcon from "@/components/icons/back";
+import { Item, updateItem } from "@/lib/features/storeSlice";
+
+type Operation = "+" | "-"
 
 export default function BuyPage() {
     const cart = useAppSelector((s) => s.persistedReducer.productsState.cart);
+
+    const dispatch = useAppDispatch()
+
+    function changeQuantity(operation: Operation, item: Item) {
+        const newValue = operation === "+" ? item.item.quantity + 1 : item.item.quantity - 1
+        if (newValue <= 0) return;
+        const newItem: Item = {
+            id: item.id,
+            item: {
+                name: item.item.name,
+                picture: item.item.picture,
+                quantity: newValue,
+                price: item.item.price
+            }
+        }
+        dispatch(updateItem(newItem));
+    }
     return (
         <section className="mt-20 mb-10 bg-white mx-2 py-5">
             <div className="flex justify-around my-5 items-center">
                 <Link href={"/main/products"}><BackIcon /></Link>
-            <h1 className="text-4xl text-center">Carrito</h1>
+                <h1 className="text-4xl text-center">Carrito</h1>
                 <div></div>
             </div>
             <div className="grid grid-cols-4 justify-center items-center text-center bg-gray-200 mx-4 my-5">
@@ -27,8 +47,23 @@ export default function BuyPage() {
                     <div key={i} className="col-span-4 grid grid-cols-4 justify-center items-center text-center justify-items-center">
                         <p>{product.item.name}</p>
                         <Image src={`https://drive.google.com/uc?id=${getImageId(product.item.picture)}`} alt="image" width={50} height={"50"}></Image>
-                        <p>{product.item.quantity}</p>
-                        <p>{product.item.quantity*product.item.price}</p>
+                        <div className="flex ">
+                            <button className="bg-green-700 hover:bg-green-400 border border-gray-300 rounded-s-full p-1 lg:p-3  focus:ring-gray-100 focus:ring-2 focus:outline-none"
+                                onClick={(e) => { changeQuantity("-", product) }}>
+                                <svg className="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
+                                    <path stroke="currentColor" d="M1 1h16" />
+                                </svg>
+                            </button>
+                            <input type="number" className="bg-gray-50 border-x-0 border-gray-300 text-center text-gray-900 focus:ring-blue-500 focus:border-blue-500 block w-10 py-1 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                value={product.item.quantity} onChange={() => { }} />
+                            <button className="bg-green-700  hover:bg-green-400 border border-gray-300 rounded-e-full p-1 lg:p-3 focus:ring-gray-100 focus:ring-2 focus:outline-none"
+                                onClick={(e) => { changeQuantity("+", product) }}>
+                                <svg className="h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                                    <path stroke="currentColor" d="M9 1v16M1 9h16" />
+                                </svg>
+                            </button>
+                        </div>
+                        <p>{product.item.quantity * product.item.price}</p>
                     </div>)}
             </div>
             <div className="w-full flex justify-center">
@@ -36,4 +71,8 @@ export default function BuyPage() {
             </div>
         </section>
     )
+}
+
+function setItem(newItem: Item) {
+    throw new Error("Function not implemented.");
 }
