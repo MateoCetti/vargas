03526a2a8e172e12472fc4a42aa3c8baf396c284
Mateo Cetti <mateocetti2000@gmail.com>
2024-09-26 22:32:30 -0300

    Change history text

--- src/app/main/about_us/page.tsx
+++ src/app/main/about_us/page.tsx
@@ -4,22 +4,23 @@ export default function AboutUsPage() {
         <section className="mt-20 mb-6 bg-white shadow">
             <div className="bg-green-900 text-center py-4 text-white text-xl">Una empresa - Un legado de familia.</div>
             <p className="text-center p-4">
-                {"Como toda empresa familiar su historia se encuentra ligada aun pasado lleno de alegrias y desafios, comienza con la llegadade los antepasados más especifico Ramón Vargas siendo española comienzos del siglo pasado. (ya en su querida españaprecisamente en Adra un pequeño puebo de Andalucia se dedicabanal comercio de frutas y verduras); al llegar a América,haciendo base en la provincia de San Juan comienzan aproducir tomates y uvas comercializando dichas mercaderias enferias de aquella pequeña provincia, ya para la decada del 40con la incorporación de Adrian y Antonio Vargas (hijos de Ramón)decidieron incurcionar en la exportación de mercaderias a laciudad de Tucumán y Córdoba, llegando asi al año 1957 sedecide comprar dos locales comerciales en aquel 'antiguo'Mercado de Abasto que se ubicaba a orillas delRío Suquia de Córdoba."}
-            </p>
-
-            <p className="text-center p-4">
-                {"Llegado el año 1982, Adrian Vargas (hijo de Adrian y sobrino de Antonio), el actual dueño se asocia a la firma de Vargas, incorporando la introducción de mercaderias del famoso y querido litoral (Entre Rios y Corrientes) de Argentina respetando calidad en los citricos, de esta manera llegamos al 88' año en que se produce el traslado al nuevo y actual Mercado de Abasto ubicando en Ruta 19 km 7 1/2."}
+                Como toda empresa familiar su historia se encuentra ligadComo toda empresa familiar, nuestra historia está entrelazada con un pasado lleno de alegrías y desafíos. Todo comienza a principios del siglo pasado con la llegada de nuestros antepasados, en particular Ramón Vargas, quien provenía de España. En su querido pueblo de Adra, en Andalucía, se dedicaban al comercio de frutas y verduras. Al llegar a América y establecerse en la provincia de San Juan, comenzaron a producir tomates y uvas, comercializando estos productos en ferias de la región.
             </p>
             <p className="text-center p-4">
-                Aqui cambian costumbres, con la llegada de nuevas tecnologias se comienzan a utilizar autoelevadores para la practica rápida de descarga y carga de mercaderias y llegando asi a más clientes con eficacia y eficiencia.
+                En la década de 1940, con la incorporación de Adrián y Antonio Vargas, hijos de Ramón, decidieron incursionar en la exportación de mercancías hacia las ciudades de Tucumán y Córdoba. En 1957, tomaron la importante decisión de adquirir dos locales comerciales en el antiguo Mercado de Abasto, ubicado a orillas del río Suquía en Córdoba.
             </p>
             <p className="text-center p-4">
-                En la ultima decada con la inclución y participación de sus hijos Lucas Vargas, Federico Vargas y Melina Vargas, pudieron incorporar nuevas ideas creando asi otro ritmo y funcionamiento de comercialización, haciendo crecer el negocio familiar incorporando nuevas mercaderias de diferentes zonas de producción, llegando asi a convirtirse en una Empresa que brinda más productos, más servicios y más calidad en sus productos.
+                En 1982, Adrián Vargas, hijo de Adrián y sobrino de Antonio, se asocia a la firma familiar, introduciendo productos del querido litoral argentino, especialmente de Entre Ríos y Corrientes, manteniendo siempre un alto estándar de calidad en los cítricos. En 1988, se realizó el traslado al nuevo y actual Mercado de Abasto, situado en la Ruta 19, km 7 1/2.
             </p>
             <p className="text-center p-4">
-                A pesar de este avance, y objetivos que se cumplieron nunca se dejara atras el como surgio esta comercialización de frutas y verduras, agradeciendo infinitamente a Dios, a las autoridades municipales, a nuestros proveedores, a nuestros empleados, a nuestra creciente clientela y al consumidor final que sin la elección, confianza y aprobación de ellos esta historia no podría ser contada.
+                Con el paso del tiempo y la llegada de nuevas tecnologías, se empezaron a utilizar autoelevadores para agilizar la carga y descarga de mercancías, lo que permitió llegar a más clientes con mayor eficacia y eficiencia.
             </p>
-
+            <p className="text-center p-4">
+                En la última década, la participación de sus hijos Lucas, Federico y Melina Vargas ha traído nuevas ideas y un renovado ritmo en la comercialización, haciendo crecer el negocio familiar e incorporando productos de diversas zonas de producción. Así, nos hemos convertido en una empresa que ofrece más productos, más servicios y una mayor calidad.
+            </p>
+            <p className="text-center p-4">
+                A pesar de estos avances y logros, nunca olvidamos nuestras raíces y cómo comenzó esta historia de comercialización de frutas y verduras. Agradecemos infinitamente a Dios, a nuestros proveedores, a nuestros empleados, a nuestra creciente clientela y al consumidor final. Sin su confianza y apoyo, esta historia no podría ser contada.
+            </p>
             <h1 className="text-xl text-center py-10">¡Gracias por todo!</h1>
         </section>
     );
