0aea56bfb8faf47a75113f8b9b92f0dc156a555e
Mateo Cetti <mateocetti2000@gmail.com>
2024-09-26 21:11:35 -0300

    Change product card size.

--- src/app/main/products/page.tsx
+++ src/app/main/products/page.tsx
@@ -7,7 +7,7 @@ export default async function productsPage() {
 
 
     return (
-        <section className="mt-20 mb-10 lg:mx-2 bg-white grid grid-cols-1 lg:grid-cols-4 justify-items-center items-center gap-6">
+        <section className="mt-20 mb-10 lg:mx-2 bg-white grid grid-cols-1 gap-2 lg:grid-cols-6 justify-items-center items-center">
             <div className="col-span-full bg-green-700 w-full text-center p-2 text-white text-2xl">Nuestros productos</div>
 
             {
--- src/app/main/products/productCard.tsx
+++ src/app/main/products/productCard.tsx
@@ -11,12 +11,12 @@ export default function ProductCard({ product}: { product: product}) {
     
     return (
         <>
-            <div className="group border border-gray bg-white lg:mx-2 lg:my-2 w-screen lg:w-full hover:bg-green-700 flex flex-col items-center cursor-pointer"
+            <div className="group border border-gray rounded rounded-xl lg:bg-white lg:my-2 p-5 w-screen lg:w-auto hover:bg-green-700 flex flex-col items-center cursor-pointer"
                 onClick={() => { router.push(`/main/products/${product.id}`) }}>
                 {/* TODO: Change to set only de google drive image ID on DB and hardcode url here. */}
-                <Image width={300} height={200} src={`https://drive.google.com/uc?id=${getImageId(product.image)}`} 
+                <Image width={200} height={200} src={`https://drive.google.com/uc?id=${getImageId(product.image)}`} 
                 alt={product.name} className="transition duration-300 ease-in-out group-hover:scale-110" />
-                <p className="text-center pt-2 pb-1 group-hover:text-white">{product.name}</p>
+                <p className="text-center group-hover:text-white">{product.name}</p>
             </div>
         </>
     )
