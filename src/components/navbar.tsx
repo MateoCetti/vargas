408b08606e1b7730f15d3d830bcb922a8870b729
Mateo Cetti <mateocetti2000@gmail.com>
2024-10-02 18:56:54 -0300

    Redirect to home page on logo click

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -17,36 +17,38 @@ const adminItems: Array<{ name: string, href: string }> = [
 
 export type itemsType = typeof menuItems;
 
-export default function Navbar({ type }: {type: "admin" | "user"}) {
+export default function Navbar({ type }: { type: "admin" | "user" }) {
 
     return (
         <nav className={`grid grid-cols-3 p-2 ${type === "admin" ? "bg-blue-500" : "bg-white"} shadow-md fixed top-0 w-screen z-40`}>
             <Drawer items={menuItems} />
-            <Image
-                src="/logo_1.png"
-                width={94}
-                height={43}
-                alt="Picture of the author" />
+            <Link href={"/"}>
+                <Image
+                    src="/logo_1.png"
+                    width={94}
+                    height={43}
+                    alt="Picture of the author" />
+            </Link>
             <ul className="hidden lg:flex flex-row justify-around mt-2.5">
                 {
-                    type === "admin" ? 
-                    
-                    adminItems.map((element, index) =>
-                        <li key={index} className="hover:font-bold hover:underline decoration-green-800 decoration-2 transition duration-300 ease-in-out hover:scale-110">
-                            <Link
-                                key={index}
-                                href={element.href}>{element.name}
-                            </Link>
-                        </li>)
-                    :
-                    
-                    menuItems.map((element, index) =>
-                        <li key={index} className="group transition duration-300 ease-in-out hover:scale-110">
-                            <Link className="group-hover:bg-green-600 p-2 rounded rounded-l group-hover:text-white"
-                                key={index}
-                                href={element.href}>{element.name}
-                            </Link>
-                        </li>)
+                    type === "admin" ?
+
+                        adminItems.map((element, index) =>
+                            <li key={index} className="hover:font-bold hover:underline decoration-green-800 decoration-2 transition duration-300 ease-in-out hover:scale-110">
+                                <Link
+                                    key={index}
+                                    href={element.href}>{element.name}
+                                </Link>
+                            </li>)
+                        :
+
+                        menuItems.map((element, index) =>
+                            <li key={index} className="group transition duration-300 ease-in-out hover:scale-110">
+                                <Link className="group-hover:bg-green-600 p-2 rounded rounded-l group-hover:text-white"
+                                    key={index}
+                                    href={element.href}>{element.name}
+                                </Link>
+                            </li>)
                 }
             </ul>
             {type === "user" && <ShoppingCart />}
