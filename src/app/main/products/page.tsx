8dbbec1eda2d17fcf40a7a84cb1ed74c3eadd58c
Mateo Cetti <mateocetti2000@gmail.com>
2024-10-29 19:38:07 -0300

    Change products page title

--- src/app/main/products/page.tsx
+++ src/app/main/products/page.tsx
@@ -1,6 +1,13 @@
+import localFont from "next/font/local";
 import db from "../../../../db";
 
 import ProductCard from "./productCard";
+
+const myFont = localFont({
+    src: '../../../../public/fonts/choco.woff2',
+    display: 'swap',
+    variable: '--font-choco',
+})
 
 export default async function productsPage() {
     const res = await db.query.products.findMany()
@@ -8,7 +15,7 @@ export default async function productsPage() {
 
     return (
         <section className="mt-20 mb-10 lg:mx-2 bg-white grid grid-cols-1 gap-2 lg:grid-cols-6 justify-items-center items-center">
-            <div className="col-span-full bg-green-700 w-full text-center p-2 text-white text-2xl">Nuestros productos</div>
+            <div className={`bg-green-900 text-center col-span-6 py-4 text-white text-xl w-full ${myFont.className}`}>Nuestros productos</div>
 
             {
                 res.map((p, i) => <ProductCard key={i} product={p} />)
