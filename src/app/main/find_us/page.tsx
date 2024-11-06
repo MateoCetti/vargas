66635bdf3700a190d350c3bb12ef3ec41cd4a50a
Mateo Cetti <mateocetti2000@gmail.com>
2024-11-06 19:20:30 -0300

    Center video

--- src/app/main/find_us/page.tsx
+++ src/app/main/find_us/page.tsx
@@ -12,7 +12,7 @@ export default function FindUsPage() {
     return (
         <section className="bg-white mt-20 mb-5">
             <div className={`bg-green-900 text-center py-4 text-white text-xl w-full ${myFont.className}`}>¿Donde encontrarnos?</div>
-            <div className="flex grid grid-cols-1 lg:grid-cols-2">
+            <div className="flex grid grid-cols-1 lg:grid-cols-2 mb-10">
                 <div className="text-justify lg:text-center p-5 self-center">
                     <p>Estamos ubicados en el mercado de abasto de la provincia de Córdoba
                         (Ruta 19, Km 7 1/2, Córdoba Argentina), mas esepcificamente hablando
@@ -28,9 +28,24 @@ export default function FindUsPage() {
                         loading="lazy"
                         referrerPolicy="no-referrer-when-downgrade" />
                 </div>
-                <div className="lg:col-span-2 lg:px-5">
-                    <iframe className="w-full h-[400px] lg:h-[720px]" src="https://www.youtube.com/embed/peV6VhmQV0o" ></iframe>
-                </div>
+
+            </div>
+            <div className="bg-green-600 relative">
+                <svg viewBox="0 0 1440 100" xmlns="http://www.w3.org/2000/svg" className="absolute -top-5 lg:-top-20">
+                    <path
+                        d="M0 43.9999C106.667 43.9999 213.333 7.99994 320 7.99994C426.667 7.99994 533.333 43.9999 640 43.9999C746.667 43.9999 853.333 7.99994 960 7.99994C1066.67 7.99994 1173.33 43.9999 1280 43.9999C1386.67 43.9999 1440 19.0266 1440 9.01329V100H0V43.9999Z"
+                        className="fill-current text-green-600"></path>
+                </svg>
+            </div>
+            <div className="relative rotate-180 -translate-y-1">
+                <svg viewBox="0 0 1440 100" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M0 43.9999C106.667 43.9999 213.333 7.99994 320 7.99994C426.667 7.99994 533.333 43.9999 640 43.9999C746.667 43.9999 853.333 7.99994 960 7.99994C1066.67 7.99994 1173.33 43.9999 1280 43.9999C1386.67 43.9999 1440 19.0266 1440 9.01329V100H0V43.9999Z"
+                        className="fill-current text-green-600"></path>
+                </svg>
+            </div>
+            <div className="lg:px-5 flex justify-center">
+                <iframe className="w-[1080px] h-[400px] lg:h-[720px]" src="https://www.youtube.com/embed/peV6VhmQV0o" ></iframe>
             </div>
         </section>
     );
